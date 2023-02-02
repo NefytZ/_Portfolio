7b13cc6a7fa0eb2ed5fa21234e96da8888fa2125
NefytZ <thyphen45@live.fr>
2023-02-02 18:51:12 +0100

    allfixtureinvue

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\FormationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index(FormationRepository $formationRepository): Response
     {
+        $formation = $formationRepository->findAll();
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formation,
         ]);
     }
 }
--- src/Controller/HobbieController.php
+++ src/Controller/HobbieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\HobbieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HobbieController extends AbstractController
 {
     #[Route('/hobbie', name: 'app_hobbie')]
-    public function index(): Response
+    public function index(HobbieRepository $hobbieRepository): Response
     {
+        $hobbie = $hobbieRepository->findAll();
         return $this->render('hobbie/index.html.twig', [
-            'controller_name' => 'HobbieController',
+            'hobbies' => $hobbie,
         ]);
     }
 }
